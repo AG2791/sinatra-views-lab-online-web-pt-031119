7b234c960e66c6bac506ad999bfb542c4ed5f2b4
AJ Guiles <ashiguiles@gmail.com>
2019-06-24 00:06:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,20 @@ class App < Sinatra::Base
 
 	get '/' do
 		erb :index
+		
 	end
-
+	
+	get '/' do
+		erb :hello
+	end
+	
+	
+	get '/' do
+		erb :goodbye
+	end
+	
+	get '/' do
+		erb :date 
+	end
 
 end
